fc4d464d40d02bb62699ddc34222b890b7e2b149
Jean Ureña <juanpedro1519@gmail.com>
2017-01-15 23:11:07 -0300

    Change Content by Description

--- db_create.py
+++ db_create.py
@@ -6,7 +6,7 @@ db.create_all()
 
 #insert
 
-db.session.add(Post(title="The Chess Flamenguist", content="A new Chess Legen has born"))
-db.session.add(Post(title="FSND", content="Full Stack Web Developer Nanodegree"))
+db.session.add(Post(title="The Chess Flamenguist", description="A new Chess Legen has born"))
+db.session.add(Post(title="FSND", description="Full Stack Web Developer Nanodegree"))
 
 db.session.commit()
