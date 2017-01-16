1ec7588b8913e14819ca2703a07541dc096cf91c
Jean Ureña <juanpedro1519@gmail.com>
2017-01-15 23:21:27 -0300

    Database Content and Static Files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import Post
 app = Flask(__name__)
 db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/welcome')
 def hello():
-	return "Welcome To Flask"
+	posts = db.session.query(Post).all()
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
