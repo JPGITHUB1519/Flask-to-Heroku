5bb714f7fb647b0c83b37c1a9e645c3b77088712
Jean Ureña <juanpedro1519@gmail.com>
2017-01-15 23:46:52 -0300

    Fixing

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from models import Post
+from models import *
 app = Flask(__name__)
 db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/welcome')
 def hello():
-	# posts = db.session.query(Post).all()
+	posts = db.session.query(Post).all()
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
